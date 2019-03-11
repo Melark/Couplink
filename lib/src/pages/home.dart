7c9779890a6e439a0bb0ee6f3cb9c478f492a70b
Mark Arrow <markarrow6@gmail.com>
2019-03-11 15:46:07 +0200

    Created clean project

---
+++ lib/src/pages/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final Widget child;
+
+  HomePage({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Couplink"),));
+  }
+}
